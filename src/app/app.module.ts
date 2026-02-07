9d3b917c0da15c953c25722099f27d7696832451
ayala-shachar <af0533124274@gmail.com>
2026-02-08 00:00:21 +0200

    Restructure project and organize modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { LayoutComponent } from './core/components/layout/layout.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  {
+    path: '',                     
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'products',
+        loadChildren: () => import('./features/product/product.module').then(m => m.ProductModule)
+      },
+      { path: '', redirectTo: 'products', pathMatch: 'full' }
+    ]
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
